98443adfedc0d534e9c2ba7cd160ceec5952f778
yesdslv <yesdslv@gmail.com>
2020-12-16 00:10:22 +0600

    Added imports in app.module.ts
    -Added MatToolbar import
    -Added MatIcon import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

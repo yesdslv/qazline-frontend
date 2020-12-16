a4887fa123532983af3ad493cbd95fddf72eb52f
yesdslv <yesdslv@gmail.com>
2020-12-17 00:54:04 +0600

    Added sidenav to body component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { BodyComponent } from './body/body.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { BodyComponent } from './body/body.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

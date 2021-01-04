a7a0673efd45a4ab95b2cbdeef4e5315f3dccb51
yesdslv <yesdslv@gmail.com>
2021-01-04 17:22:23 +0600

    Added imports to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TreeComponent } from './tree/tree.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
+    TreeComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,6 +28,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
+    MatTreeModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
